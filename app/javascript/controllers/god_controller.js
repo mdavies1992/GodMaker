8dea624692fc25bc4cc03981f63228443964a200
Matthew Davies <m.davies.work@gmail.com>
2024-06-04 18:49:06 +0900

    RNG or select JS now works, devise added, show page outlined

--- app/javascript/controllers/god_controller.js
+++ app/javascript/controllers/god_controller.js
@@ -2,16 +2,40 @@ import { Controller } from "@hotwired/stimulus"
 
 
 export default class extends Controller {
-  static targets = ["name"];
+  static targets = ["name", "alignment", "domain"];
 
   connect() {
     console.log("God Connected");
   }
 
   namecheck(event) {
-    const name = event.currentTarget.value
-    console.log(name);
+    const nischecked = event.currentTarget.checked
+    console.log(nischecked);
+    if (nischecked){
+      this.nameTarget.classList.remove("d-none")
+    } else {
+      this.nameTarget.classList.add("d-none")
+    }
    }
 
+   alignmentcheck(event) {
+    const aischecked = event.currentTarget.checked
+    console.log(aischecked);
+    if (aischecked){
+      this.alignmentTarget.classList.remove("d-none")
+    } else {
+      this.alignmentTarget.classList.add("d-none")
+    }
+   }
+
+   domaincheck(event) {
+    const ischecked = event.currentTarget.checked
+    console.log(ischecked);
+    if (ischecked){
+      this.domainTarget.classList.remove("d-none")
+    } else {
+      this.domainTarget.classList.add("d-none")
+    }
+   }
 
 }
