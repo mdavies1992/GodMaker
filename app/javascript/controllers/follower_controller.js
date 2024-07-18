e19b8fd8bd7f982f6a94173eb4925db2b94d1214
Matthew Davies <m.davies.work@gmail.com>
2024-07-18 12:07:01 +0900

    js follower controller setup

---
+++ app/javascript/controllers/follower_controller.js
@@ -0,0 +1,50 @@
+import { Controller } from "@hotwired/stimulus"
+
+
+export default class extends Controller {
+  static targets = ["name", "alignment", "race", "profession"];
+
+  connect() {
+    console.log("Follower Connected");
+  }
+
+  namecheck(event) {
+    const nischecked = event.currentTarget.checked
+    console.log(nischecked);
+    if (nischecked){
+      this.nameTarget.classList.remove("d-none")
+    } else {
+      this.nameTarget.classList.add("d-none")
+    }
+   }
+
+   alignmentcheck(event) {
+    const aischecked = event.currentTarget.checked
+    console.log(aischecked);
+    if (aischecked){
+      this.alignmentTarget.classList.remove("d-none")
+    } else {
+      this.alignmentTarget.classList.add("d-none")
+    }
+   }
+
+   racecheck(event) {
+    const ischecked = event.currentTarget.checked
+    console.log(ischecked);
+    if (ischecked){
+      this.raceTarget.classList.remove("d-none")
+    } else {
+      this.raceTarget.classList.add("d-none")
+    }
+   }
+
+   professioncheck(event) {
+    const pischecked = event.currentTarget.checked
+    console.log(pischecked);
+    if (pischecked){
+      this.professionTarget.classList.remove("d-none")
+    } else {
+      this.professionTarget.classList.add("d-none")
+    }
+   }
+}
---
+++ db/migrate/20240718024005_add_random_selections_to_followers.rb
@@ -0,0 +1,8 @@
+class AddRandomSelectionsToFollowers < ActiveRecord::Migration[7.1]
+  def change
+    add_column :followers, :random_name, :boolean
+    add_column :followers, :random_alignment, :boolean
+    add_column :followers, :random_species, :boolean
+    add_column :followers, :random_profession, :boolean
+  end
+end
